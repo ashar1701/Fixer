bffa4a73ea04d17c78f856d88cf6dcc7ccaccfc9
bfaheem <bfaheem@uwo.ca>
2025-03-06 13:41:54 -0500

    fixing filename casing issue

---
+++ src/components/Listing.js
@@ -0,0 +1,17 @@
+import { 
+    Card,
+    CardContent,
+    Typography
+ } from '@mui/material';
+
+const Listing = () => {
+    return <div className='listing'>
+        <Card>
+            <CardContent>
+                <Typography variant="h1">Listing</Typography>
+            </CardContent>
+        </Card>
+    </div>
+}
+
+export default Listing;
---
+++ src/components/temp.js
@@ -0,0 +1,9 @@
+import Listing from 'Listing.js';
+
+const ListingList = () => {
+    return <div className='listing-list'>
+        <Listing/>
+    </div>
+}
+
+export default ListingList;

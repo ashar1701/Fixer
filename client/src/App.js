a4a8ca26393408e233c1ec92537fbd0d02fafc92
bfaheem <bfaheem@uwo.ca>
2025-03-08 14:05:52 -0500

    Added a listing list page, created temporary navbar, and login/signUp buttons lead to respective pages.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { Button, Typography, Container } from "@mui/material";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ListingList from "./components/ListingList.js";
+import NavBar from "./components/NavBar.js";
+import Login from "./Login.js";
+import SignUp from "./SignUp.js";
 
 function App() {
   return (
-    <div className="container">
-      <ListingList />
-    </div>
+    <Router>
+      <NavBar />
+      <div className="container">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<ListingList />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
